05548e81b233f7b629bb2b1ad62ab67e9fc8dbb6
Lukeguy334 <lhs57@usf.edu>
2025-02-13 21:38:11 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -6,3 +6,13 @@ return `Total invouce: $${total.toFixed(2)}`;
 }
 console.log(calculateInvoice(100, 0.08, 5)); // Total invouce: $103.00
 console.log(calculateInvoice(500, 0.1, 20)); // Total invouce: $530.00
+
+// Task 2
+const calculateHourlyWage = function(salary, hoursPerWeek) {
+    //Calculate hourly wage
+    let hourlyWage = salary / (hoursPerWeek *52);
+    return `Hourly wage: $${hourlyWage.toFixed(2)}`;
+};
+    //Test cases
+    console.log(calculateHourlyWage(52000, 40)); // Hourly wage: $25.00
+    console.log(calculateHourlyWage(75000, 35)); // Hourly wage: $41.21
